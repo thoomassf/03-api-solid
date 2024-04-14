9cf5398fd2621d3361ab5d0d7df4e7ca3f063d9a
Thomas Farias <thomasfarias19@gmail.com>
2024-04-14 18:00:27 -0300

    create authenticate use-case and test

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -1,35 +1,34 @@
-import { FastifyRequest, FastifyReply } from "fastify"
-import { z } from "zod"
-import { RegisterUseCase } from "@/use-cases/register"
-import { PrismaUsersRepository } from "@/repositories/prisma/prisma-users-repository"
-import { UserAlreadyExistsError } from "@/use-cases/errors/user-already-exists-error"
+import { FastifyRequest, FastifyReply } from "fastify";
+import { z } from "zod";
+import { RegisterUseCase } from "@/use-cases/register";
+import { PrismaUsersRepository } from "@/repositories/prisma/prisma-users-repository";
+import { UserAlreadyExistsError } from "@/use-cases/errors/user-already-exists-error";
 
-
-export async function register (request: FastifyRequest, reply: FastifyReply) {
+export async function register(request: FastifyRequest, reply: FastifyReply) {
   const registerBodySchema = z.object({
     name: z.string(),
     email: z.string().email(),
     password: z.string().min(6),
-  })
+  });
 
-  const { name, email, password } = registerBodySchema.parse(request.body)
+  const { name, email, password } = registerBodySchema.parse(request.body);
 
   try {
-    const usersRepository = new PrismaUsersRepository()
-    const registerUseCase = new RegisterUseCase(usersRepository)
+    const usersRepository = new PrismaUsersRepository();
+    const registerUseCase = new RegisterUseCase(usersRepository);
 
     await registerUseCase.execute({
       name,
-      email, 
+      email,
       password,
-    })
+    });
   } catch (err) {
     if (err instanceof UserAlreadyExistsError) {
-      return reply.status(409).send({ message: err.message })
+      return reply.status(409).send({ message: err.message });
     }
 
-    throw err
+    throw err;
   }
 
-  return reply.status(201).send()
+  return reply.status(201).send();
 }
--- src/repositories/prisma/prisma-users-repository.ts
+++ src/repositories/prisma/prisma-users-repository.ts
@@ -1,5 +1,5 @@
 import { prisma } from "@/lib/prisma";
-import { Prisma } from "@prisma/client"
+import { Prisma } from "@prisma/client";
 import { UsersRepository } from "../users-repository";
 
 export class PrismaUsersRepository implements UsersRepository {
@@ -8,16 +8,16 @@ export class PrismaUsersRepository implements UsersRepository {
       where: {
         email,
       },
-    })
+    });
 
-    return user
+    return user;
   }
 
   async create(data: Prisma.UserCreateInput) {
-    const user =await prisma.user.create({
+    const user = await prisma.user.create({
       data,
-    })
+    });
 
-    return user
-  } 
+    return user;
+  }
 }
