3c10318b5d0e970436ef78249d161a71dedd591e
Thomas Farias <thomasfarias19@gmail.com>
2024-04-17 20:56:59 -0300

    create check-ins tests e2e

--- src/http/controllers/check-ins/create.ts
+++ src/http/controllers/check-ins/create.ts
@@ -16,7 +16,7 @@ export async function create(request: FastifyRequest, reply: FastifyReply) {
     }),
   });
 
-  const { gymId } = createCheckInParamsSchema.parse(request.body);
+  const { gymId } = createCheckInParamsSchema.parse(request.params);
   const { latitude, longitude } = createCheckInBodySchema.parse(request.body);
 
   const createCheckInUseCase = makeCheckInUseCase();
--- src/http/controllers/check-ins/history.ts
+++ src/http/controllers/check-ins/history.ts
@@ -7,7 +7,7 @@ export async function history(request: FastifyRequest, reply: FastifyReply) {
     page: z.coerce.number().min(1).default(1),
   });
 
-  const { page } = checkInHistoryQuerySchema.parse(request.body);
+  const { page } = checkInHistoryQuerySchema.parse(request.query);
 
   const fetchUserCheckInHistoryUseCase = makeFetchUserCheckInsHistoryUseCase();
 
