15dafbed0f39247ee9b1745b67068ffca4988c44
Thomas Farias <thomasfarias19@gmail.com>
2024-04-17 00:47:29 -0300

    create test environment

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,10 @@
-import { defineConfig } from 'vitest/config'
-import tsconfigPath from 'vite-tsconfig-paths'
+import { defineConfig } from "vitest/config";
+import tsconfigPath from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [tsconfigPath()],
-})
+  test: {
+    environmentMatchGlobs: [["src/http/controllers/**", "prisma"]],
+    dir: "src", // Essa linha
+  },
+});
